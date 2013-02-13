972bc3c8f6ca3209dc6c5cc0073854ab66dd485b
narendrashetty <mailnarendra15@gmail.com>
2013-02-13 19:49:17 +0530

    bootstrap initialization

--- application/views/home.php
+++ application/views/home.php
@@ -1,50 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<title>CMR MCA</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>css/style.css">
+  <head>
+    <meta charset="utf-8">
+    <title>Template &middot; Bootstrap</title>
+
+
+    <!-- Le styles -->
+    <link href="<?php echo base_url(); ?>css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>css/responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
 	
-	<link href='http://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic' rel='stylesheet' type='text/css'>
-
+	
     <script src="<?php echo base_url(); ?>js/jquery-1.7.1.min.js"></script>
     <script src="<?php echo base_url(); ?>js/jquery.easing.1.3.js"></script>
     <script src="<?php echo base_url(); ?>js/tms-0.4.1.js"></script>
     <script src="<?php echo base_url(); ?>js/jcarousellite.js" type="text/javascript"></script>
-</head>
-<body>
-<div class="main">
-<!--==============================header=================================-->
-<header>
-	<div class="head-content">
-		<div class="container_12">
-			<div class="grid_12">
-				<div class="fright">
-					<div class="clear"></div>
-					<nav>
-						<ul class="sf-menu">
-							<li class="current"><a href="<?php echo base_url(); ?>index.html">Home</a></li>
-							<li><a href="<?php echo base_url(); ?>">admission</a>
-							</li>
+  </head>
+
+  <body>
+
+    <div class="container">
+	<header>
+		<div class="head-content">
+			<div class="row-fluid">
+			
+				<div class="span6 pull-right nav-align">
+						<ul class="nav nav-pills nav-property">
+							<li class="active"><a href="<?php echo base_url(); ?>index.html">Home</a></li>
+							<li><a href="<?php echo base_url(); ?>">admission</a></li>
 							<li><a href="<?php echo base_url(); ?>">programs</a></li>
 							<li><a href="<?php echo base_url(); ?>">about us</a></li>
 							<li><a href="<?php echo base_url(); ?>">news</a></li>
 							<li><a href="<?php echo base_url(); ?>">Contacts</a></li>
-						</ul>    
-					</nav>
+						</ul>    				
 				</div>
-				<div class="extra-wrap">
-					<img src=<?php echo base_url(); ?>images/logo-small.png />
-					
-				</div>
+				
+				<div class="span3 pull-left logo-align">
+				<img src="images/logo-small.png" />
 			</div>
 		</div>
-		<div class="clear"></div>
-	</div>
-
-	            <div id="slide">
+		
+			            <div id="slide">
 				<div class="slider">
 					<ul class="items">
 				    <li><img src="<?php echo base_url(); ?>images/slide.jpg" alt="" />
@@ -74,89 +77,13 @@
 					</ul>
 				</div>
 			 </div>
+		
+	</header>
 	
-</header>
-
-<div class="mca">
-Department Of <span style="color:#a6b032;"> MCA</span>
-</div>
-
-<div class="blocks wrapper">
-	<div class="container_12">
-		          <div class="carousel-box">
-            <span>Upcoming Events</span>
-            <a id="prev"></a>
-	  	    <a id="next"></a>
-            <div class="carousel">
-                <ul>
-                    <li>
-                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
-                        <a href="#">Class aptent taciti<br> sociosqu ad litora torquent.</a>
-                    </li>
-                    <li>
-                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
-                        <a href="#">Phasellus pharetra<br>interdum ante sit amettinc. </a>
-                    </li>
-                    <li>
-                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
-                        <a href="#">Aenean pulvinar porta velit <br> quis scelerisque.</a>
-                    </li>
-                    <li>
-                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
-                        <a href="#">dolore magna aliquam <br> erat volutpat.</a>
-                    </li>
-                    <li>
-                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
-                        <a href="#">Class aptent taciti<br> sociosqu ad litora torquent.</a>
-                    </li>
-                    <li>
-                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
-                        <a href="#">Phasellus pharetra<br>interdum ante sit amettinc. </a>
-                    </li>
-                    <li>
-                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
-                        <a href="#">Aenean pulvinar porta velit <br> quis scelerisque.</a>
-                    </li>
-                    <li>
-                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
-                        <a href="#">dolore magna aliquam <br> erat volutpat.</a>
-                    </li>
-                </ul>
-            </div>
-	</div>
-</div>
-</div>
-<section id="content">
-	<div class="container_12">
-		<div class="grid_4">
-			<div class="block">
-				<p class="font-3">Block 1</p>
-				<ul class="top-1 list">
-					<li><a href="#">List1</a></li>
-					<li><a href="#">List2</a></li>
-					<li><a href="#">List3</a></li>
-					<li><a href="#">List3</a></li>
-					<li><a href="#">List4</a></li>
-				</ul>
-			</div>
-		</div>
-		<div class="grid_4">
-			<h2>Block 2</h2>
-		</div>
-		<div class="grid_4">
-			<h2>Block 3</h2>
-		</div>
-	</div>
-	<div class="clear"></div>
-</section> 
-<div class="wrapper">
-	<div class="container_12">
-		<div class="grid_12">
-		All Rights Reserved &copy;		
-		</div>
-	</div>
-</div>
-	    <script>
+	
+	
+    </div> <!-- /container -->
+	<script>
       $(window).load(function(){
           $('.slider')._TMS({
               show:0,
@@ -189,6 +116,5 @@ Department Of <span style="color:#a6b032;"> MCA</span>
 		 });
       })
     </script>
-	
-</body>
+  </body>
 </html>
