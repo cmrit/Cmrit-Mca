b4a2459562a3a0ffe5088a53178f698c4e559026
narendrashetty <mailnarendra15@gmail.com>
2013-02-16 22:18:01 -0800

    initialized new about view

---
+++ application/views/about.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>CMR MCA</title>
+
+
+    <!-- Le styles -->
+    <link href="<?php echo base_url(); ?>css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>css/responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+	
+		<link href='http://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'>
+	
+    <script src="<?php echo base_url(); ?>js/jquery-1.7.1.min.js"></script>
+    <script src="<?php echo base_url(); ?>js/jquery.easing.1.3.js"></script>
+    <script src="<?php echo base_url(); ?>js/tms-0.4.1.js"></script>
+    <script src="<?php echo base_url(); ?>js/jcarousellite.js" type="text/javascript"></script>
+  </head>
+
+  <body>
+
+    <div class="container">
+	<header>
+		<div class="head-content">
+			<div class="row-fluid">
+			
+				<div class="span6 pull-right nav-align">
+						<ul class="nav nav-pills nav-property">
+							<li class="active"><a href="<?php echo base_url(); ?>home">Home</a></li>
+							<li><a href="<?php echo base_url(); ?>about">about us</a></li>
+							<li><a href="<?php echo base_url(); ?>">admission</a></li>
+							<li><a href="<?php echo base_url(); ?>">programs</a></li>
+							<li><a href="<?php echo base_url(); ?>">news</a></li>
+							<li><a href="<?php echo base_url(); ?>">Contacts</a></li>
+						</ul>    				
+				</div>
+				
+				<div class="span3 pull-left logo-align">
+				<img src="images/logo-small.png" />
+			</div>
+		</div>
+		
+			            <div id="slide">
+				<div class="slider">
+					<ul class="items">
+				    <li><img src="<?php echo base_url(); ?>images/slide.jpg" alt="" />
+						 <div class="banner">
+                             <div class="banner-inner">
+                                <span><strong>Career</strong> opportunities</span>
+                                <p>Vivamus et metus non lorem ullamcorper aliquam tempus molestie nunc.</p>
+                             </div>
+                         </div>
+					</li>
+				    <li><img src="<?php echo base_url(); ?>images/slide-1.jpg" alt="" />
+						<div class="banner">
+							<div class="banner-inner">
+							        <span><strong>Knowledge</strong>  is power!</span>
+							        <p>Vivamus et metus non lorem ullamcorper aliquam tempus molestie nunc.</p>
+							</div>
+						</div>
+					</li>
+                    <li><img src="<?php echo base_url(); ?>images/slide-2.jpg" alt="" />
+						<div class="banner">
+							<div class="banner-inner">
+							        <span><strong>It's never </strong>too late to study!</span>
+							        <p>Vivamus et metus non lorem ullamcorper aliquam tempus molestie nunc.</p>
+							</div>
+						</div>
+					</li>
+					</ul>
+				</div>
+			 </div>
+		
+	</header>
+	
+	<div class="mca">
+Department Of <span style="color:#a6b032;"> MCA</span>
+</div>
+
+<div class="blocks">
+	<div class="span10">
+		          <div class="carousel-box">
+            <span>Upcoming Events</span>
+            <a id="prev"></a>
+	  	    <a id="next"></a>
+            <div class="carousel">
+                <ul>
+                    <li>
+                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
+                        <a href="#">Class aptent taciti<br> sociosqu ad litora torquent.</a>
+                    </li>
+                    <li>
+                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
+                        <a href="#">Phasellus pharetra<br>interdum ante sit amettinc. </a>
+                    </li>
+                    <li>
+                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
+                        <a href="#">Aenean pulvinar porta velit <br> quis scelerisque.</a>
+                    </li>
+                    <li>
+                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
+                        <a href="#">dolore magna aliquam <br> erat volutpat.</a>
+                    </li>
+                    <li>
+                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
+                        <a href="#">Class aptent taciti<br> sociosqu ad litora torquent.</a>
+                    </li>
+                    <li>
+                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
+                        <a href="#">Phasellus pharetra<br>interdum ante sit amettinc. </a>
+                    </li>
+                    <li>
+                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
+                        <a href="#">Aenean pulvinar porta velit <br> quis scelerisque.</a>
+                    </li>
+                    <li>
+                        <img src="<?php echo base_url(); ?>images/page-img.jpg" alt="">
+                        <a href="#">dolore magna aliquam <br> erat volutpat.</a>
+                    </li>
+                </ul>
+            </div>
+</div>
+</div>
+</div>
+
+<div class="content row-fluid">
+		<div class="span4 well">
+				<p class="font-3">Block 1</p>
+				<ul class="top-1 list">
+					<li><a href="#">List1</a></li>
+					<li><a href="#">List2</a></li>
+					<li><a href="#">List3</a></li>
+					<li><a href="#">List3</a></li>
+					<li><a href="#">List4</a></li>
+				</ul>
+		</div>
+		<div class="span4">
+			<h2>Block 2</h2>
+		</div>
+		<div class="span3">
+			<h2>Block 3</h2>
+		</div>
+</div>
+
+
+<footer>
+	<div class="container_12">
+		<div class="span8">
+		All Rights Reserved &copy;		
+		</div>
+	</div>
+</footer>
+
+	
+    </div> <!-- /container -->
+	<script>
+      $(window).load(function(){
+          $('.slider')._TMS({
+              show:0,
+              pauseOnHover:false,
+              playBu:false,
+              duration:1000,
+              preset:'fade', 
+              pagination:true,//'.pagination',true,'<ul></ul>'
+              pagNums:false,
+              slideshow:6000,
+              numStatus:false,
+              banners:'fromRight',
+		      waitBannerAnimation:false,
+			  progressBar:false
+          })
+		  $(function() {
+             $(".carousel").jCarouselLite({	
+                  btnNext: "#next",
+                  btnPrev: "#prev",
+                  easing:"easeOutBack",
+                  speed: 700
+             });
+          });		
+		  $('.cycle-div').cycle({ 
+		    fx:     'fade', 
+		    speed:  1000, 
+		    timeout: 0, 
+		    next:   '#next2', 
+		    prev:   '#prev2' 
+		 });
+      })
+    </script>
+  </body>
+</html>
