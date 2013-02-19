dd1818fa06743a35714625d6cd19130a24b4d04a
narendrashetty <mailnarendra15@gmail.com>
2013-02-18 17:58:03 -0800

    adujsted main links position

--- application/views/about.php
+++ application/views/about.php
@@ -9,14 +9,11 @@
     <link href="<?php echo base_url(); ?>css/style.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>css/responsive.css" rel="stylesheet">
 
-		<link href='http://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
+	<!--	<link href='http://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-	
-    <script src="<?php echo base_url(); ?>js/jquery-1.7.1.min.js"></script>
-    <script src="<?php echo base_url(); ?>js/jquery.easing.1.3.js"></script>
-    <script src="<?php echo base_url(); ?>js/tms-0.4.1.js"></script>
-    <script src="<?php echo base_url(); ?>js/jcarousellite.js" type="text/javascript"></script>
+	-->
+
   </head>
 
   <body id="p2">
@@ -40,7 +37,8 @@
 				<div class="span3 pull-left logo-align">
 				<img src="images/logo-small.png" />
 			</div>
-		</div>		
+		</div>	
+		</div>
 	</header>
 	
 	<div class="mca">
