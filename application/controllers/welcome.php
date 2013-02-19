49495a135769f96b4890601e7c6a3a215f951969
narendrashetty <mailnarendra15@gmail.com>
2013-02-18 18:23:05 -0800

    defined function for contact view

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,12 +19,17 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view("home");
+		$this->load->view("home", $this->data);
 	}
 	
 	public function about()
 	{
-		$this->load->view("about");
+		$this->load->view("about", $this->data);
+	}
+	
+	public function contact()
+	{
+		$this->load->view("contact", $this->data);
 	}
 	
 }
