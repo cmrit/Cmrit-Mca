40b838f7f157292d03683c538b4452d7b8d2b0e7
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-03-17 01:26:22 -0800

    added all the guest sessions

--- guest.php
+++ guest.php
@@ -94,6 +94,253 @@ The speakers are usually those with a proven expertise in the Industry, who can 
 Their insights are truly valuable to the students who get immense ideas about what to expect from the Industry and as to how to make themselves more employable. Also, they get a clear idea about how to chart their careers so as to balance their work and personal issues, helping them lead a satisfying life. 
 
 			</p>
+			<div class="clear"></div>
+			<div class="span4 well" style="text-align:center;">
+			<p><strong>Guest Lectures - 2006</strong></p>
+			<table class="table table-bordered">
+			<tr>
+			<td>Date</td>
+			<td>Topic</td>
+			</tr>
+			<tr>
+			<td>13.03.06</td>
+			<td>Data Warehousing Principles</td>
+			</tr>
+			<tr>
+			<td>24.04.06</td>
+			<td>E-Security</td>
+			</tr>
+			<tr>
+			<td>20.11.06</td>
+			<td>Survey of Software Engineering Methods</td>
+			</tr>
+			<tr>
+			<td>25.11.06</td>
+			<td>Corporate Communication</td>
+			</tr>
+			</table>
+			</div>
+			
+			<div class="span4 well" style="text-align:center;height:242px;">
+			<p><strong>Guest Lectures - 2007</strong></p>
+			<table class="table table-bordered">
+			<tr>
+			<td>Date</td>
+			<td>Topic</td>
+			</tr>
+			<tr>
+			<td>29.03.07</td>
+			<td>Real Time System</td>
+			</tr>
+			<tr>
+			<td>06.10.07</td>
+			<td>Unix Programming</td>
+			</tr>
+			<tr>
+			<td>03.11.07</td>
+			<td>Socket Programming,JDBC,Swings </td>
+			</tr>
+			</table>
+			</div>
+			
+			
+						<div class="span4 well" style="text-align:center;">
+			<p><strong>Guest Lectures - 2008</strong></p>
+			<table class="table table-bordered">
+			<tr>
+			<td>Date</td>
+			<td>Topic</td>
+			</tr>
+			<tr>
+			<td>09.02.08</td>
+			<td>Application of DBMS</td>
+			</tr>
+			<tr>
+			<td>06.10.07</td>
+			<td>Unix Programming</td>
+			</tr>
+			<tr>
+			<td>01.03.08</td>
+			<td>Design and Analysis of Algorithm, Unix Administration</td>
+			</tr>
+			<tr>
+			<td>15.03.08</td>
+			<td>System Engineering</td>
+			</tr>
+			<tr>
+			<td>05.04.08</td>
+			<td>Future Trends in Technologies</td>
+			</tr>
+			<tr>
+			<td>22.08.08</td>
+			<td>Embedded System</td>
+			</tr>
+			<tr>
+			<td>20.09.08</td>
+			<td>System Programming</td>
+			</tr>
+			</table>
+			</div>
+			
+			
+			<div class="span4 well" style="text-align:center;height:373px;">
+			<p><strong>Guest Lectures - 2009</strong></p>
+			<table class="table table-bordered">
+			<tr>
+			<td>Date</td>
+			<td>Topic</td>
+			</tr>
+			<tr>
+			<td>11.02.09</td>
+			<td>Web Programming</td>
+			</tr>
+			<tr>
+			<td>14.02.09</td>
+			<td>Web Programming</td>
+			</tr>
+			<tr>
+			<td>28.02.09</td>
+			<td>Web Programming</td>
+			</tr>
+			<tr>
+			<td>04.04.09</td>
+			<td>Analysis & Design of Algorithms</td>
+			</tr>
+			<tr>
+			<td>18.04.09</td>
+			<td>J2EE</td>
+			</tr>
+			<tr>
+			<td>18.04.09</td>
+			<td>Management Information System</td>
+			</tr>
+			<tr>
+			<td>18.04.09</td>
+			<td>UNIX Programming</td>
+			</tr>
+			</table>
+			</div>
+			
+			<div class="span4 well" style="text-align:center;">
+			<p><strong>Guest Lectures - 2010</strong></p>
+			<table class="table table-bordered">
+			<tr>
+			<td>Date</td>
+			<td>Topic</td>
+			</tr>
+			<tr>
+			<td>06.03.10</td>
+			<td>Web Application Development</td>
+			</tr>
+			<tr>
+			<td>20.03.10</td>
+			<td>Enterprise Java Beans</td>
+			</tr>
+			<tr>
+			<td>24.04.10</td>
+			<td>Web Programming</td>
+			</tr>
+			<tr>
+			<td>04.04.09</td>
+			<td>Supply Chain Management</td>
+			</tr>
+			<tr>
+			<td>21.08.10</td>
+			<td>Data Base Management System</td>
+			</tr>
+			<tr>
+			<td>04.09.10</td>
+			<td>Web Programming 2.0</td>
+			</tr>
+			<tr>
+			<td>09.10.10</td>
+			<td>Dot Net</td>
+			</tr>
+			</table>
+			</div>
+			
+			<div class="span4 well" style="text-align:center;">
+			<p><strong>Guest Lectures - 2011</strong></p>
+			<table class="table table-bordered">
+			<tr>
+			<td>Date</td>
+			<td>Topic</td>
+			</tr>
+			<tr>
+			<td>19.02.11</td>
+			<td>Advance Java</td>
+			</tr>
+			<tr>
+			<td>05.03.11</td>
+			<td>Web Programming</td>
+			</tr>
+			<tr>
+			<td>19.03.11</td>
+			<td>Management Information Systems</td>
+			</tr>
+			<tr>
+			<td>26.03.11</td>
+			<td>Soft Skill</td>
+			</tr>
+			<tr>
+			<td>26.03.11</td>
+			<td>Rich Internet Application</td>
+			</tr>
+			<tr>
+			<td>16.08.11</td>
+			<td>How to start developing an application using .NET</td>
+			</tr>
+			<tr>
+			<td>25.08.11</td>
+			<td>How to develop an Application using PhP</td>
+			</tr>
+			<tr>
+			<td>08.09.11</td>
+			<td>Android Application </td>
+			</tr>
+			<tr>
+			<td>08.10.11</td>
+			<td>Introduction to Cloud Computing</td>
+			</tr>
+			</table>
+			</div>
+			
+			<div class="span4 well" style="text-align:center;">
+			<p><strong>Guest Lectures - 2012</strong></p>
+			<table class="table table-bordered">
+			<tr>
+			<td>Date</td>
+			<td>Topic</td>
+			</tr>
+			<tr>
+			<td>18.10.12</td>
+			<td>Guest Lecture on Project Development Using RAD</td>
+			</tr>
+			<tr>
+			<td>03/10/12</td>
+			<td>Guest Lecture on Java</td>
+			</tr>
+			</table>
+			</div>
+			
+			<div class="span4 well" style="text-align:center;">
+			<p><strong>Guest Lectures - 2013</strong></p>
+			<table class="table table-bordered">
+			<tr>
+			<td>Date</td>
+			<td>Topic</td>
+			</tr>
+			<tr>
+			<td>23.02.13</td>
+			<td>Development of Enterprise Application using J2EE</td>
+			</tr>
+			<tr>
+			<td>15.03.13</td>
+			<td>IT Application Web Enabled in Education and Health Care</td>
+			</tr>
+			</table>
+			</div>
+			
 			
 			<div class="clear"></div>
 		
