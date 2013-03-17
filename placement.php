db9e09e5eebe29621db5316f92c209087a1e7182
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-03-17 00:43:07 -0800

    added all placed students photo

---
+++ placement.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>CMR MCA</title>
+
+
+    <!-- Le styles -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+	
+	<script src="js/jquery-1.7.1.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/tms-0.4.1.js"></script>
+    <script src="js/jcarousellite.js" type="text/javascript"></script>
+	<script src="js/slider-dropdown.js"></script>
+
+	<link href='http://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+  </head>
+
+  <body id="p2">
+
+    <div class="container">
+	<header>
+		<div class="head-content">
+			<div class="row-fluid">
+			
+				<div class="span9 pull-right nav-align">
+						<ul class="nav nav-pills nav-property">
+							<li><a href="index.php">Home</a></li>
+							<li id="about"><a href="#" class="account">about us</a>
+								<div class="submenu">
+								<ul class="root">
+									<li ><a href="about.php">Vision</a></li>
+									<li ><a href="about.php">Mission</a></li>
+									<li ><a href="about.php">About Department</a></li>
+									<li ><a href="about.php">Our Staffs</a></li>
+									<li ><a href="course.php">Course Details</a></li>
+								<li ><a href="about.php">Admission</a></li>
+								</ul>
+								</div>
+							</li>
+							<li class="active"><a href="placement.php">Placement</a></li>
+							<li id="beyond"><a href="#" class="account">Beyond Syllabus</a>
+								<div class="submenu">
+								<ul class="root">
+									<li ><a href="lifeskill.php">Life Skills</a></li>
+									<li ><a href="extra.php">Extra Curricular</a></li>
+									<li ><a href="counseling.php">Counseling</a></li>
+									<li ><a href="guest.php">Guest Lectures</a></li>
+									<li ><a href="icp.php">ICP Process</a></li>
+									<li ><a href="newsletter.php">News letter</a></li>
+									<li ><a href="social.php">Social Concern</a></li>
+									<li ><a href="technical.php">Technical Fest</a></li>
+								</ul>
+								</div>
+								</li>
+							<li><a href="research.php">Research Initiatives</a></li>
+							<li><a href="achievements.php">Achievements</a></li>
+							<li><a href="contact.php">Contacts</a></li>
+						</ul>    								
+				</div>
+				
+				<div class="span2 pull-left logo-align">
+				<img src="images/logo-small.png" />
+			</div>
+		</div>	
+		</div>
+	</header>
+	
+	<div class="mca">
+Department Of <span style="color:#a6b032;"> MCA</span>
+</div>
+
+<div class="content">
+<div class="span10">
+			<h2>Placements</h2>
+			<hr />
+<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/tanuja.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Tanuja M Giri</h4>
+	  <h5>Batch : 2009</h4>
+	  <p>
+	  Software Engineer at Wipro
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	
+<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/parmar.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Parmar Pawan</h4>
+	  <h5>Batch : 2009</h4>
+	  <p>
+	  Software Engineer at Razor Sight
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	
+<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/chetan.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Chetan K J</h4>
+	  <h5>Batch : 2010</h4>
+	  <p>
+	  Software Engineer at HP
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/rudersh.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Rudersh Navathi</h4>
+	  <h5>Batch : 2010</h4>
+	  <p>
+	  Software Engineer at Wipro
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/deepak.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Deepak M Joshi</h4>
+	  <h5>Batch : 2010</h4>
+	  <p>
+	  Software Engineer at NeoBytes India
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/ravi.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Ravi Chandra</h4>
+	  <h5>Batch : 2010</h4>
+	  <p>
+	  Software Engineer at HP
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/divya.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Divya S M</h4>
+	  <h5>Batch : 2010</h4>
+	  <p>
+	  Software Engineer at HP
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	
+<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/namrata.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Namrata Ahuja</h4>
+	  <h5>Batch : 2011</h4>
+	  <p>
+	  Software Engineer at TCS
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	
+<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/anita.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Anitha D</h4>
+	  <h5>Batch : 2011</h4>
+	  <p>
+	  Software Engineer at Dell
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>		
+	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/dhirendra.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Dhirendra Kumar</h4>
+	  <h5>Batch : 2011</h4>
+	  <p>
+	  Software Engineer at Razor Sight
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>		
+	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/jyostna.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Jyostna Mishra</h4>
+	  <h5>Batch : 2011</h4>
+	  <p>
+	  Software Engineer at TCS
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>		
+	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/suprith.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Suprith B K</h4>
+	  <h5>Batch : 2012</h4>
+	  <p>
+	  Software Engineer at Maventic
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>		
+	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/manoj.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Manoj M Bhardwaj</h4>
+	  <h5>Batch : 2012</h4>
+	  <p>
+	  Web Developer at Retail On
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	
+	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/swaroop.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Swaroop S M</h4>
+	  <h5>Batch : 2012</h4>
+	  <p>
+	  Software Engineer at Dell
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	
+	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/arun.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Arun Kolhapur</h4>
+	  <h5>Batch : 2012</h4>
+	  <p>
+	  Software Engineer at Youdyog
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	
+	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/krunal.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Krunal Patel</h4>
+	  <h5>Batch : 2012</h4>
+	  <p>
+	  Software Engineer at Cyber Forensic
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	
+	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/ramakrishna.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Ramakrishna</h4>
+	  <h5>Batch : 2012</h4>
+	  <p>
+	  Software Engineer at Cyber Forensic
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>	
+	<div class="span4">
+			<ul class="media-list well">
+  <li class="media">
+    <a class="pull-left" href="#">
+      <img class="media-object" src="images/riya.jpg">
+    </a>
+    <div class="media-body">
+      <h4 class="media-heading">Riya Saha</h4>
+	  <h5>Batch : 2012</h4>
+	  <p>
+	  Software Engineer at Cyber Forensic
+	  </p>
+    </div>
+  </li>
+</ul>
+	</div>		
+			<div class="clear"></div>
+		
+		</div>
+</div>
+
+
+
+<footer>
+	<div class="container_12">
+		<div class="span8">
+		All Rights Reserved &copy;		
+		</div>
+	</div>
+</footer>
+
+	
+    </div> <!-- /container -->
+  </body>
+</html>
