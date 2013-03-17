7d3268032c4778a4561e830ebad73c1fe3e1f6d0
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-03-17 08:11:49 -0800

    added all details on course

--- course.php
+++ course.php
@@ -14,8 +14,6 @@
     <script src="js/tms-0.4.1.js"></script>
     <script src="js/jcarousellite.js" type="text/javascript"></script>
 	<script src="js/slider-dropdown.js"></script>
-	<script src="js/jquery.lightbox-0.5.pack.js"></script>
-	<script src="js/bootstrap.js"></script>
 
 	<link href='http://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'>
@@ -79,80 +77,111 @@ Department Of <span style="color:#a6b032;"> MCA</span>
 
 <div class="content">
 <div class="span10">
-<div class="span6 pull-right">
-			<h2>About Department</h2>
+
+			<h2>Course Details</h2>
 			<hr />
-			<p>
-				The selection is based on the CET ( Karnataka Management Aptitude Test) for filling up the seats of Government quota in the institute. </p>
-<p>For the Management quota, the selection is based on the score obtained by the student in any of the tests like PGCET and the aggregate percentage marks obtained in the qualifying degree examination. </p>
-<p>The minimum prescribed eligibility criteria by VTU and STATE GOVT OF KARNATAKA will be strictly adhered to.</p>
-			
-			
-			<div class="span6" id="staff-box">
-					          <div class="carousel-box">
-            <h2>Our Staffs</h2>
-			<hr />
-            <a id="prev"></a>
-	  	    <a id="next"></a>
-            <div class="carousel">
-                <ul>
-                    <li>
-                        <img src="images/p2-page-img.jpg" alt="">
-                        <a href="#">Name 1</a>
-                    </li>
-                    <li>
-                        <img src="images/p2-page-img.jpg" alt="">
-                        <a href="#">Name 2</a>
-                    </li>
-                    <li>
-                        <img src="images/p2-page-img.jpg" alt="">
-                        <a href="#">Name 3</a>
-                    </li>
-                    <li>
-                        <img src="images/p2-page-img.jpg" alt="">
-                        <a href="#">Name 4</a>
-                    </li>
-                    <li>
-                        <img src="images/p2-page-img.jpg" alt="">
-                        <a href="#">Name 5</a>
-                    </li>
-                    <li>
-                        <img src="images/p2-page-img.jpg" alt="">
-                        <a href="#">Name 6</a>
-                    </li>
-                    <li>
-                        <img src="images/p2-page-img.jpg" alt="">
-                        <a href="#">Name 7</a>
-                    </li>
-                    <li>
-                        <img src="images/p2-page-img.jpg" alt="">
-                        <a href="#">Name 8</a>
-                    </li>
-                </ul>
-            </div>
-</div>
-</div>
-			
-		</div>
-			<h2>Vision</h2>
-			<hr />
-			<p>
-			To create, build and provide value added educational services through teaching, training, 
-			research, consultancy and entrepreneurship within and beyond the curriculum, by means of quality 
-			infrastructure, material and human resources to attain intellectual heights.
+<p><strong>
+CORE Subjects
+</strong>
+</p>
+<p><strong>
+Problem Solving using C–</strong>Algorithms and Flowcharts, Constants, Variables and Data Types, Operators and Expressions, Managing Input and Output Operations, Control Statements, Loop Control Structures, Arrays, Character Arrays and Strings, User Defined Functions, Structures and Unions, Pointers, File Management in C, Dynamic Memory Allocation, The Preprocessor
+</p>
+<p><strong>
+Fundamentals of Computer Organization- </strong>Number Systems and Logic Gates, Combining Logic Gates and Arithmetic Circuits, Basic Structure of Computer,Machine Instruction and Programmes, Input/Output Organization, Memory Systems, Arithmetic Unit                                                                                                
+</p>
+<p><strong>
+Introduction to Unix- </strong>General Purpose Utilities, Navigating the File System, Handling Ordinary Files ,The vi editor, Basic File Attributes, Security and File Permission, Shell Programming, The Process, Customizing the Environment, More file attributes, Simple filters, Filters using Regular Expression and the grep Family, Essential Shell Programming, Essential System Administration, awk-Advanced Filters, Advanced Shell Programming, Advanced vi
+</p>
+<p><strong>
+Business Data Processing with COBOL- </strong>Introduction, Identification, Environment and Data Division, Procedure Division and Basic Verbs, Different Types of Clauses and other Verbs, Computation and Decision Making, Array Processing and Table Handling, Sequential File Processing, Sorting and Merging of Files, Indexed and Relative File Processing, Report Writer, and Subroutines 
+</p>
+<p><strong>
+Object Oriented Programming in C++ - </strong>Introduction, Classes & Objects, Inheritance, Virtual functions, Polymorphism, I/O System Basics, File I/0, Exception Handling, STL		
+</p>
+<p><strong>
+Data structures Using C - </strong>Basic Concepts, Arrays and Structures, Stacksand Queues, Linked Lists, Trees, Graphs, Priority Queues, Efficient Binary Search Trees
+</p>
+<p><strong>
+Systems Programming - </strong>Machine Architecture, Assemblers, Loaders and Linkers, Editors And Debugging Systems, Macro Processor, Lex and Yacc
+</p>
+<p><strong>
+Computer Networks - </strong>Foundation, Direct Link, Packet Switching, Internetworking, End –to-End Protocols; Resource Allocation Issues, Applications                                                                                                                                        
+</p>
+<p><strong>
+Programming with Java-  </strong>An Overview of Java, Introducing Classes, Inheritance, Multithreaded Programming, Enumeration, Autoboxing, Exploring java.lang, Networking, The Applet Class  
+</p>
+<p><strong>
+Database Management System - </strong>Introduction, Entity-Relationship Model, 	Relational Model and Relational Algebra, SQL, Database Design, Transaction Management
+</p>
+<p><strong>
+Operating Systems - </strong>Introduction to Operating Systems, System structures, Process Management, Process Synchronization, Deadlocks,  Memory Management, File System, Implementation of File System,  Secondary Storage Structures, Protection,  Case Study: The Linux Operating System        
+</p>
+<p><strong>
+Topics in Enterprise Architectures – I - </strong>JDBC Object, JDBC and Embedded SQL	, Servlets, Servlets – Sessions, Java Server Pages (JSP), Java Beans, Enterprise Java Beans, J2ME	
+</p>
+<p><strong>
+Software Engineering  -</strong>Overview, Software Processes, Requirements, System models, Project Management, Software Design, Development, Verification and Validation, Management 
+</p>
+<p><strong>
+Web Programming - </strong>Fundamentals of Web, XHTML, CSS, Javascript, Javascript and HTML Documents, Dynamic Documents with Javascript, XML, Perl, CGI Programming, PHP, Ruby, Rails	
+</p>
+<p><strong>
+Design and Analysis of Algorithms- </strong>Introduction, Divide and Conquer, The Greedy Method, Dynamic Programming, Decrease-and-Conquer Approaches, Space-Time Tradeoffs, Limitations of Algorithmic Power and Coping with them, Coping with Limitations of Algorithmic Power, PRAM Algorithms
+</p>
+<p><strong>
+Object-Oriented Modeling and Design Patterns- </strong>Introduction, Modeling Concepts, class Modeling, Advanced Class Modeling, State Modeling, Advanced State Modeling, Interaction Modeling, Process Overview, System Conception, Domain Analysis, Application Analysis, System Design, Class Design, Implementation Modeling, Legacy Systems, Design Patterns, Idioms
+</p>
+<p><strong>
+System simulation and Modeling - </strong>Introduction, General Principles, Simulation Software, Statistical Models in Simulation, Queuing Models, Random-Number Generation, Random-Variate Generation, Input Modeling, Estimation of Absolute Performance, Verification, Calibration, and Validation; Optimization
+</p>
+<p><strong>
+Topics in Enterprise Architectures –II- </strong>The philosophy of .NET, Building C# Applications, Language Fundamentals, Object- Oriented Programming with C#, Exceptions and Object Lifetime, Interfaces and Collections, Callback Interfaces, Delegates, and Events, Advanced Techniques, Understanding .NET Assembles.
+</p>
+<p><strong>
+Professional Communication & Ethics–</strong>Professional / Technical Communication, Basics of Technical Communication, Barriers to Communication, Technology in Communication, Active Listening, Effective Presentation Strategies, Group Communication, Research paper, Dissertation and Thesis, Ethics:An Overview of Ethics, Ethics for IT Professionals and IT users, Privacy Software Development, Employer/Employee Issues, Case Studies and Discussion
+</p>
+<p><strong>
+Management Information Systems–</strong>Systems Engineering, Information and Knowledge, Introduction of MIS, Strategic Management of Business, Development of MIS, Developing Business/IT Strategies/IT Solutions, Business Process Re-Engineering, Technology of Information System, Decision Making and DSS, Data resource Management, Electronic Business systems, Enterprise Business Systems, Client Server Architecture and E-business Technology     
+
+</p>
+<p><strong>
+Digital Electronics</strong>
+</p>
+<p><strong>
+Operations Research</strong>
+</p>
+<p><strong>
+Discrete Mathematics</strong>
+</p>
+<p><strong>
+Electives being Offered</strong>
+Computer Graphics and Visualization, UNIX system Programming, Multimedia Systems, Pattern Recognition, Principles of User Interface Design, Advanced Computer Networks, Information Retrieval, Data Warehousing and Data Mining, Supply Chain Management, Network Management, Compiler Design, Software Architectures, Information & Network Security, Software Testing, Services Oriented Architecture, Wireless Networks and Mobile Computing, Storage Area Networks, Web 2.0 & Rich Internet Applications
+</p>
+<p><strong>
+Laboratories
+</strong>
+<ul>
+<li>C Programming Laboratory</li>
+<li>Unix Laboratory</li>
+<li>IT and Digital Electronics Laboratory</li>
+<li>COBOL Programming Laboratory</li>
+<li>Data Structures Using C Laboratory</li>
+<li>Object Oriented Programming with C++ Laboratory</li>
+<li>Systems Programming Laboratory</li>
+<li>Java Programming  Laboratory</li>
+<li>DBMS Laboratory</li>
+<li>J2EE Laboratory</li>
+<li>Web programming Laboratory</li>
+<li>Algorithms Laboratory</li>
+<li>Software Design Laboratory</li>
+<li>.Net Laboratory</li>
+<li>Mini Project</li>
+</ul>
 			</p>
 			
 			<div class="clear"></div>
 		
-			<h2>Mission Statement</h2>
-			<hr />
-			<p>
-				<ul>
-				<li>Give the community genuine, talented, upto date, motivated and trained professionals.</li>
-<li>Develop all institutions as model institutions which will enable the students or the parents to realise their ambition / dream in a more structured and positive manner.</li>
-<li>Inculcate the right values, scientific temperament and social commitment amongst the students and ensure they become right partners of progress.</li>
-				</ul>
-			</p>
 		</div>
 </div>
 
