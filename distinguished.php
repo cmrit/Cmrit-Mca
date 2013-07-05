9aad91ca88b86a31e6fc3feb0fd9065971f2d2c1
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-07-05 16:45:20 +0530

    added new file alumni

---
+++ distinguished.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Distinguished Alumni</title>
+
+
+    <!-- Le styles -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+	
+	<script src="js/jquery-1.7.1.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/tms-0.4.1.js"></script>
+    <script src="js/jcarousellite.js" type="text/javascript"></script>
+	<script src="js/slider-dropdown.js"></script>
+
+	<link href='http://fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+  </head>
+
+  <body id="p2">
+
+    <div class="container">
+	<header>
+		<div class="head-content">
+			<div class="row-fluid">
+			
+				<div class="span9 pull-right nav-align">
+						<ul class="nav nav-pills nav-property">
+							<li><a href="index.php">Home</a></li>
+							<li id="about"><a href="#" class="account">about us</a>
+								<div class="submenu">
+								<ul class="root">
+									<li ><a href="about.php">Vision</a></li>
+									<li ><a href="about.php">Mission</a></li>
+									<li ><a href="about.php">About Department</a></li>
+									<li ><a href="about.php">Our Staffs</a></li>
+									<li ><a href="course.php">Course Details</a></li>
+								<li ><a href="admission.php">Admission</a></li>
+								<li ><a href="contact.php">Contact Us</a></li>
+								</ul>
+								</div>
+							</li>
+							
+							<li id="beyond"><a href="#" class="account">Beyond Syllabus</a>
+								<div class="submenu">
+								<ul class="root">
+									<li ><a href="lifeskill.php">Life Skills</a></li>
+									<li ><a href="extra.php">Extra Curricular</a></li>
+									<li ><a href="counseling.php">Counseling</a></li>
+									<li ><a href="guest.php">Guest Lectures</a></li>
+									<li ><a href="icp.php">ICP Process</a></li>
+									<li ><a href="newsletter.php">NewsLetter</a></li>
+									<li ><a href="social.php">Social Concern</a></li>
+									<li ><a href="technical.php">Technical Fest</a></li>
+								</ul>
+								</div>
+								</li>
+								<li><a href="placement.php">Placement</a></li>
+							<li><a href="research.php">Research Initiatives</a></li>
+							<li><a href="achievements.php">Achievements</a></li>
+							<li class="active"><a href="alumni.php">Distinguished Alumni</a></li>
+						</ul>    								
+				</div>
+				
+				<div class="span2 pull-left logo-align">
+				<img src="images/logo-small.png" />
+			</div>
+		</div>	
+		</div>
+	</header>
+	
+	<div class="mca">
+Department Of <span style="color:#a6b032;"> MCA</span>
+</div>
+
+<div class="content">
+<div class="span10">
+			<h2>Distinguished Alumni</h2>
+			<hr />
+<table class="table table-bordered">
+<tr><td width="50px">SL NO</td><td width="250px">NAME</td><td>PROFILE</td></tr>
+ <tr><td>1</td><td>Sandhya Mohan</td><td>Senior Consultant - Oracle Weblogic Server</td></tr>
+ <tr><td>2</td><td>V Bharath Kumar</td><td>Technical Services Specialist at IBM</td></tr>
+ <tr><td>3</td><td>Omkar Singh</td><td>Works at IBM Global Technology Services</td></tr>
+ <tr><td>4</td><td>Durga Singh Raghwa</td><td>Quality Analyst Engineer at Sling Media Pvt. Ltd</td></tr>
+ <tr><td>5</td><td>Shobhit Srivastava</td><td>Former Operations Senior Specialist at IBM India Pvt Ltd</td></tr>
+ <tr><td>6</td><td>Ashwini Sampagon</td><td>Works at ANZ Australia</td></tr>
+ <tr><td>7</td><td>Sai Prasad</td><td>Storage Engineer at Hewlett-Packard</td></tr>
+ <tr><td>8</td><td>Vimal Kumar Shukla</td><td>QA Team Lead at IG Group</td></tr>
+ <tr><td>9</td><td>Md. Dizaur Rohman </td><td>Senior Software Engineer at Oracle</td></tr>
+ <tr><td>10</td><td>Mohan Kumar</td><td>Lead Engineer at HCL Technologies</td></tr>
+ <tr><td>11</td><td>Rama Devi</td><td>Software Engineer at HCL Technologies</td></tr>
+ <tr><td>12</td><td>Hasna Haridas</td><td>System Engineer at IBM India</td></tr>
+ <tr><td>13</td><td>Swetha BC</td><td>Developer at NIC</td></tr>
+ <tr><td>14</td><td>Pooja Srinivas</td><td>Software Engineer at Igate Patni and SSE at IGate</td></tr>
+ <tr><td>15</td><td>Shafeer CH</td><td>Software Engineer at Hewlett-Packard</td></tr>
+ <tr><td>16</td><td>Sunny John</td><td>Software Engineer at HP</td></tr>
+ <tr><td>17</td><td>Lad Neerajkumar</td><td>Consultant Logica</td></tr>
+ <tr><td>18</td><td>Melwin</td><td>Asst. Professor at St.Joseph's College of Arts and Science,Bangalore</td></tr>
+ <tr><td>19</td><td>Sudheer N</td><td>Operations Professional at IBM</td></tr>
+ <tr><td>20</td><td>Pawan Parmar</td><td>Systems Development Associate at ACS Inc, A Xerox Company</td></tr>
+ <tr><td>21</td><td>Vinay H G</td><td>Systems Development Associate at ACS Inc, A Xerox Company</td></tr>
+ <tr><td>22</td><td>Manjunatha Patil</td><td>Works at WNS Global Services</td></tr>
+ <tr><td>23</td><td>Balasaheb Umadi</td><td>Associate Software Engineer at Infosys</td></tr>
+ <tr><td>24</td><td>Kishore Barman</td><td>Software Engineer at TCS</td></tr>
+ <tr><td>25</td><td>Mohd. Ishaq</td><td>Software Engineer at TCS</td></tr>
+ <tr><td>26</td><td>Sujatha.S G</td><td>Software Engineer at TCS</td></tr>
+ <tr><td>27</td><td>Rahat Tabassum</td><td>Software Engineer at Wipro technologies</td></tr>
+ <tr><td>28</td><td>Chitranj Barman</td><td>Software Engineer at IBM</td></tr>
+ <tr><td>29</td><td>Mohan Kumar.M </td><td>Software Engineer at Capgemini</td></tr>
+ <tr><td>30</td><td>Tanuja Giri</td><td>Software Engineer at Wipro technologies</td></tr>
+ <tr><td>31</td><td>Prashant Kumar</td><td>Software Engineer at Wipro technologies</td></tr>
+ <tr><td>32</td><td>Rajni Yadav</td><td>Software Engineer at Wipro technologies</td></tr>
+ <tr><td>33</td><td>Renu Sharma</td><td>Works at CGI's Official Page</td></tr>
+ <tr><td>34</td><td>Ranjan Kalita</td><td>Works at Neobytes Softwares PVT. Ltd</td></tr>
+ <tr><td>35</td><td>Farooq Shaiks</td><td>Works at IBM Daksh and Nippon Express</td></tr>
+ <tr><td>36</td><td>Prem Singh</td><td>Worked at Suksh technology pvt ltd</td></tr>
+ <tr><td>37</td><td>Animesh Kalita</td><td>Works at Faculty of Computer Engineering in CIPET, GHY. and Central Institute of Plastics Engineering and Technology</td></tr>
+ <tr><td>38</td><td>Ravichandra J</td><td>Software Engineer at HP</td></tr>
+ <tr><td>39</td><td>Chetan K J</td><td>Software Engineer at HP</td></tr>
+ <tr><td>40</td><td>Divya S</td><td>Software Engineer at HP</td></tr>
+ <tr><td>41</td><td>Naresh Kumar P</td><td>Software Engineer at HP</td></tr>
+ <tr><td>42</td><td>jonus joseph</td><td>Software Engineer at ELEVATE</td></tr>
+ <tr><td>43</td><td>Shivaraj RH</td><td>Senior PHP Developer at Permeative Technologies Pvt Ltd</td></tr>
+ <tr><td>44</td><td>Abhishek kumar</td><td>SAP CRM Technical Associate Consultant at Maventec</td></tr>
+ <tr><td>45</td><td>Sumith Kumar</td><td>SAP CRM Technical Associate Consultant at Maventec</td></tr>
+ <tr><td>46</td><td>Manas Ranjan</td><td>Junior Analyst at WNS Global Services</td></tr>
+ <tr><td>47</td><td>P Sivananda</td><td>Junior Analyst at WNS Global Services</td></tr>
+ <tr><td>48</td><td>Namrata  Ahuja</td><td>Associate Software Engineer at Tata Consultancy Services</td></tr>
+ <tr><td>49</td><td>Jyotsna  Misra</td><td>Associate Software Engineer at Tata Consultancy Services</td></tr>
+ <tr><td>50</td><td>Anitha D</td><td>Software Engineer at Dell</td></tr>
+ <tr><td>51</td><td>Dhirendra Tanti</td><td>Software Engineer at Ernst & Young </td></tr>
+ <tr><td>52</td><td>Amit Kumar Rajendra Kumar</td><td>Software Engineer at Patni Computers</td></tr>
+ <tr><td>53</td><td>Bablu Kumar Saw</td><td>Systems Development Associate at ACS Inc, A Xerox Company</td></tr>
+</table>
+
+			
+			<div class="clear"></div>
+		
+		</div>
+</div>
+
+
+
+<footer>
+	<div class="container_12">
+		<div class="span8">
+		All Rights Reserved &copy;		
+		</div>
+	</div>
+</footer>
+
+	
+    </div> <!-- /container -->
+  </body>
+</html>
